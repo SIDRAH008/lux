84a672a4c107eccdf9a2b7afe9e61e271000aadd
Sidra <sdrrzq@gmail.vom>
2024-01-27 15:04:34 +0500

    motgaes

--- src/pages/mortgage.jsx
+++ src/pages/mortgage.jsx
@@ -1,6 +1,36 @@
-const Mortgage =()=>{
-  return(
-    <></>
-  )
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import React, { useState } from "react";
+import Result from "../component/MortageComponents/Result";
+import SliderSelect from "../component/MortageComponents/SliderSelect";
+import TenureSelect from "../component/MortageComponents/SliderSelect";
+
+function Mortgage() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  });
+
+  console.log(data);
+
+  return (
+    <div className="Mortgage">
+      <Container maxWidth="xl" sx={{ marginTop: 4 }}>
+        <Grid container spacing={5} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <SliderSelect data={data} setData={setData} />
+            <TenureSelect data={data} setData={setData} />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Result data={data} />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
+
 export default Mortgage;
