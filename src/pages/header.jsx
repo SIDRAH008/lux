6c84cd7607af3e9ad5bb480e6dfcab6684b74a78
Sidra <sdrrzq@gmail.vom>
2024-02-02 15:54:00 +0500

    moblie res

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,28 +1,41 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Nav, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar as BootstrapNavbar, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 import "./index.css";
 import logo from "./lux.png";
+
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const handleMobileMenuToggle = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
   return (
     <div className="Header">
       <div className="leftnav">
         <img src={logo} alt="" />
       </div>
-      <div className="navlinksBar">
+
+      {/* Mobile menu icon */}
+      <div className="mobile-menu-icon" onClick={handleMobileMenuToggle}>
+        <div className={`bar ${showMobileMenu ? "open" : ""}`}></div>
+        <div className={`bar ${showMobileMenu ? "open" : ""}`}></div>
+        <div className={`bar ${showMobileMenu ? "open" : ""}`}></div>
+      </div>
+
+      <div className={`navlinksBar ${showMobileMenu ? "show" : ""}`}>
         <NavLink to="/" className="navlinks">
           Home
         </NavLink>
-        
         <NavLink to="/properties" className="navlinks">
           OFF Plan
         </NavLink>
         {/* Add more dropdown items as needed */}
-     
-      
         <NavLink to="/secendory" className="navlinks">
-         Secendary
+          Secendary
         </NavLink>
         <NavLink to="/service" className="navlinks">
           Services
@@ -37,12 +50,11 @@ const Navbar = () => {
           Login
         </NavLink>
         <NavLink to="/signup" className="navlinks">
-          Sigup
+          Signup
         </NavLink>
         <NavLink to="/chatbot" className="navlinks">
           Chatbot
         </NavLink>
-       
         <NavLink to="/createlisting" className="navlinks">
           Add Your Property
         </NavLink>
